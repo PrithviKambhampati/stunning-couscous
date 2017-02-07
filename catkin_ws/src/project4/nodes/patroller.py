4c701c658dac33e8aeaa5c1ab3f15ec124d059e5
spartanhaden <spartanhaden@gmail.com>
2017-02-07 04:17:16 -0500

    Added random spawning

--- catkin_ws/src/project4/nodes/patroller.py
+++ catkin_ws/src/project4/nodes/patroller.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python
 import rospy
+import roslaunch
+import time
+import random
 
 
 def patroller():
-    pass
+    package = 'gazebo_ros'
+    executable = 'spawn_model'
+    arguements = '-urdf -model jackal -param robot_description '
+    spawn_location = [  '-x 0 -y 0 -z 1.0',
+                        '-x 0 -y 7 -z 1.0',
+                        '-x 7 -y 7 -z 1.0',
+                        '-x 7 -y 0 -z 1.0']
+
+    location = random.choice(spawn_location)
+    #location = '-x 0 -y 7 -z 1.0'
+    arguements += location
+
+    time.sleep(10)
+
+    node = roslaunch.core.Node(package, executable, args=arguements)
+
+    launch = roslaunch.scriptapi.ROSLaunch()
+    launch.start()
+
+    process = launch.launch(node)
+
+    print('Spawned at ' + location)
+    print(process.is_alive())
+
+    time.sleep(3)
+    process.stop()
 
 
 if __name__ == '__main__':
